931ef99593eca1dbc9144beb1692b6d4879f280b
shayim520 <51888292+shayim520@users.noreply.github.com>
2019-06-18 23:59:57 -0400

    Update classIntro.java

--- src/note/classIntro.java
+++ src/note/classIntro.java
@@ -4,6 +4,7 @@ public class classIntro {
 	public static void main(String[] args) {
 		//mailm said this is variable
 		//uptade on github, also you need to pull it on eclipce
+		////uptade 2nd time on github, also you need to pull it on eclipce
 		int i = 5;
 		System.out.println(i);
 		
